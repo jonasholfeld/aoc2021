0c9e100f8839255aefb307c2a52c83a9aa2c8349
Jonas Holfeld <klatschpauke@googlemail.com>
2021-12-03 18:07:39 +0100

    refactoring

--- day3.py
+++ day3.py
@@ -1,29 +1,25 @@
 import json
-import pprint
 
 with open('input_day3.json') as file:
     powerconsumption = json.load(file)['powerconsumption']
 
 bits = {}
 
-for index in range(0, 12):
-    bits[index] = {
-        0: 0,
-        1: 0
-    }
+for index in range(0, len(powerconsumption[0])):
+    bits[index] = { 0: 0, 1: 0 }
 
 for number in powerconsumption:
     for index in range(0, len(number)):
         bits[index][int(number[index])] +=1
 
 gammarate = ''
-epsilon = ''
+epsilonrate = ''
 
-for index in range(0, 12):
+for index in range(0, len(powerconsumption[0])):
     gammarate += str(0 if bits[index][0] > bits[index][1] else 1)
-    epsilon += str(0 if bits[index][0] < bits[index][1] else 1)
+    epsilonrate += str(0 if bits[index][0] < bits[index][1] else 1)
 
-#print(int(gammarate, 2) * int(epsilon, 2))
+print(int(gammarate, 2) * int(epsilonrate, 2))
 
 oxygen = powerconsumption
 co2 = powerconsumption
@@ -42,28 +38,23 @@ def bitcriteria_co2(number, index, bitdict):
 
 def countbits(array):
     bitdict = {}
-    for index in range(0, 12):
-        bitdict[index] = {
-            0: 0,
-            1: 0
-        }
+    for index in range(0, len(array[0])):
+        bitdict[index] = { 0: 0, 1: 0 }
     for number in array:
         for index in range(0, len(number)):
             bitdict[index][int(number[index])] +=1
     return bitdict
 
-for index in range(0, 12):
+for index in range(0, len(oxygen[0])):
     bitdict = countbits(oxygen)
     if(len(oxygen) == 1):
         break
     oxygen = list(filter(lambda number: bitcriteria_oxygen(number, index, bitdict), oxygen))
 
-for index in range(0, 12):
+for index in range(0, len(co2[0])):
     bitdict = countbits(co2)
     if(len(co2) == 1):
         break
     co2 = list(filter(lambda number: bitcriteria_co2(number, index, bitdict), co2))
 
-
-#print(co2)
 print(int(oxygen[0], 2) * int(co2[0], 2))
